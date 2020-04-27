ecb2d0bd9217bf9aebea9d7177fc729e436582ae
sameer <mohdsameer7408@gmail.com>
2020-04-27 16:43:24 +0530

    Updated Settings

--- social/settings.py
+++ social/settings.py
@@ -21,15 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8=p1x-&p@(3n&l8-xzf^0*el8n4d0ae_kyqm5wvyxlbu_wew-h'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 DEBUG = True
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['mypythonblogapp.herokuapp.com']
+ALLOWED_HOSTS = ['mypythonblogapp.herokuapp.com']
 
 
 # Application definition
@@ -152,6 +150,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # django_heroku.settings(locals())
