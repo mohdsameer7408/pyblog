864e36591889602135284039a47503e56361caaa
sameer <mohdsameer7408@gmail.com>
2020-04-27 15:39:34 +0530

    initial commit

--- social/settings.py
+++ social/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'social.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
